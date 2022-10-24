1b25e224062cb136e2f74a635211c9421bf2dc5c
unknown <salehb.professional@gmail.com>
2022-10-24 07:34:11 +0400

    Update AuthForm.js

--- frontend/src/components/AuthForm/AuthForm.js
+++ frontend/src/components/AuthForm/AuthForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import css from './AuthForm.module.css';
 import axios from 'axios';
@@ -7,21 +7,29 @@ import axios from 'axios';
 
 function AuthForm() {
   const [isSignup, setIsSignup] = useState(false);
-  const userNameRef = useRef('');
-  const emailRef = useRef('');
-  const passwordRef = useRef('');
-  const passwordConfirmRef = useRef('');
+  const [enteredUserName, setEnteredUserName] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPasswordConfirm, setEnteredPasswordConfirm] = useState('');
   const dispatchFn = useDispatch();
+  // INPUT HANDLERS
+  const enteredUserNameHandler = (e) => setEnteredUserName(e.target.value);
+  const enteredEmailHandler = (e) => setEnteredEmail(e.target.value);
+  const enteredPasswordHandler = (e) => setEnteredPassword(e.target.value);
+  const enteredPasswordConfirmHandler = (e) => setEnteredPasswordConfirm(e.target.value);
+  // INPUT HANDLERS
 
+  // SIGNUP STATE
   const toogleLogin = () => setIsSignup(false);
   const toogleSignup = () => setIsSignup(true);
+  // SIGNUP STATE
 
   function submitHandler(e) {
     e.preventDefault();
-    const data = { email: emailRef.current.value, password: passwordRef.current.value };
+    const data = { email: enteredEmail, password: enteredPassword };
     if (isSignup) {
-      data.passwordConfirm = passwordConfirmRef.current.value;
-      data.userName = userNameRef.current.value;
+      data.passwordConfirm = enteredPasswordConfirm;
+      data.userName = enteredUserName;
     }
     const url = `http://localhost:8000/api/users/${isSignup ? 'signup' : 'login'}`;
     axios.post(url, data).then((res) => {
@@ -37,10 +45,10 @@ function AuthForm() {
         <span onClick={toogleLogin}>Login</span>
       </div>
       <form onSubmit={submitHandler}>
-        {isSignup && <input type='text' placeholder='User name' ref={userNameRef} />}
-        <input type='email' placeholder='Email' ref={emailRef} />
-        <input type='password' placeholder='Password' ref={passwordRef} />
-        {isSignup && <input type='password' placeholder='Confirm password' ref={passwordConfirmRef} />}
+        {isSignup && <input type='text' placeholder='User name' onChange={enteredUserNameHandler} />}
+        <input type='email' placeholder='Email' onChange={enteredEmailHandler} />
+        <input type='password' placeholder='Password' onChange={enteredPasswordHandler} />
+        {isSignup && <input type='password' placeholder='Confirm password' onChange={enteredPasswordConfirmHandler} />}
         <button type='submit'>Submit</button>
       </form>
     </div>
